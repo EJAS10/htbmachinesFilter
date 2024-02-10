59ab51ef12f3054ebb680d4e97660c8dccb992f5
Emmanuel Almanzar Sanchez <76236121+EJAS10@users.noreply.github.com>
2024-02-10 11:34:06 -0400

    agregando script para en base a la ip y al cird me diga la mascara y el total de host

---
+++ SubNet/GetSubNet.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+
+calculate_subnet_mask() {
+    local cidr="$1"
+    local mask=""
+    for (( i=0; i<32; i++ )); do
+        if (( i < cidr )); then
+            mask+="1"
+        else
+            mask+="0"
+        fi
+    done
+    echo "$((2#${mask:0:8}))"."$((2#${mask:8:8}))"."$((2#${mask:16:8}))"."$((2#${mask:24:8}))"
+}
+
+
+calculate_total_hosts() {
+    local cidr="$1"
+    echo "$((2**(32-cidr))-2)"
+}
+
+
+if [[ $# -ne 2 ]]; then
+    echo "Uso: $0 <ip_address> <CIDR>"
+    exit 1
+fi
+
+ip_address="$1"
+cidr="$2"
+
+
+if ! [[ "$ip_address" =~ ^([0-9]{1,3}\.){3}[0-9]{1,3}$ ]]; then
+    echo "Dirección IP inválida."
+    exit 1
+fi
+
+if ! [[ "$cidr" =~ ^[0-9]+$ ]] || (( cidr < 0 || cidr > 32 )); then
+    echo "Notación CIDR inválida."
+    exit 1
+fi
+
+subnet_mask=$(calculate_subnet_mask "$cidr")
+
+IFS='.' read -r -a ip_parts <<< "$ip_address"
+IFS='.' read -r -a mask_parts <<< "$subnet_mask"
+network_address=""
+for (( i=0; i<4; i++ )); do
+    network_address+="$(( ${ip_parts[$i]} & ${mask_parts[$i]} ))"
+    if (( i < 3 )); then
+        network_address+="."
+    fi
+done
+
+first_address="${network_address%.*}.1"
+last_address="${network_address%.*}.254"
+
+total_hosts=$(calculate_total_hosts "$cidr")
+
+echo "Dirección de red: $network_address"
+echo "Máscara de subred: $subnet_mask"
+echo "Rango de direcciones IP: $first_address - $last_address"
+echo "Número total de hosts: $total_hosts"
+echo "--------------------------------------------------------"
+echo "CIRD 1-8 sin clase Subnet x.0.0.0"
+echo "CIRD 9-16 Clase A Subnet 255.x.0.0"
+echo "CIRD 17-24 Clase B Subnet 255.255.x.0"
+echo "CIRD 25-32 Clase C Subnet 255.255.255.x"
